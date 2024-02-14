9996cf30fbc131e47bf73f9da3d398e68a09ce17
Adrian ANDRE <aandre@henix.fr>
2024-02-14 10:11:10 +0100

    change cuf

--- src/test/java/test.java
+++ src/test/java/test.java
@@ -1,8 +1,11 @@
 import org.junit.jupiter.api.Test;
+import org.opentestfactory.exception.ParameterException;
+import org.opentestfactory.util.ParameterService;
 
 public class test {
     @Test
-    public void testing(){
-        System.out.println("This is a test");
+    public void testing() throws ParameterException {
+        String param = ParameterService.INSTANCE.getString("IT_CUF_owl");
+        System.out.println(param);
     }
 }
